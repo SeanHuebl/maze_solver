4546741da56a931cddfffab553963075ce5b1870
SeanH <85364282+SeanHuebl@users.noreply.github.com>
2024-09-23 17:50:50 +0000

    created window class, line class, and point class. added methods to enable drawing of a line on the canvas. testing for drawing the line still in main()

---
+++ main.py
@@ -0,0 +1,20 @@
+from line import Line, Point
+from window import Window
+
+
+
+def main():
+    win = Window(800, 600)
+    
+    # Testing the draw_line method
+    point_1 = Point(0,0)
+    point_2 = Point(400, 400)
+    line = Line(point_1, point_2)
+    win.draw_line(line, 'black')
+
+    win.wait_for_close()
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ window.py
@@ -0,0 +1,31 @@
+from tkinter import Tk, BOTH, Canvas
+
+from line import Line
+
+class Window():
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+        self.__root_widget = Tk()
+        self.__root_widget.title = 'root'
+        self.__root_widget.geometry(f"{width}x{height}")
+        self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas = Canvas(width=self.width, height=self.height)
+        self.canvas.pack()
+        self.running = False
+    
+    def redraw(self) -> None:
+        self.__root_widget.update_idletasks()
+        self.__root_widget.update()
+
+    def wait_for_close(self):
+        self.running = True
+
+        while self.running == True:
+            self.redraw()
+    
+    def close(self) -> None:
+        self.running = False
+
+    def draw_line(self, line: Line, fill_color: str) -> None:
+        line.draw(self.canvas, fill_color)
